e446d01fb5daf715c7e6446945c0ff0b1b32295a
Ore Landau <Ore4444@gmail.com>
2013-07-22 17:08:14 +0300

    Excluding CSS

--- package.js
+++ package.js
@@ -7,8 +7,6 @@ Package.on_use(function (api) {
   api.add_files([
     'lib/jquery.dataTables.js',
 	'lib/DT_bootstrap.js',
-    'lib/jquery.dataTables.css',
-	'lib/DT_bootstrap.css',
     'lib/images/back_disabled.png',
     'lib/images/back_enabled.png',
 	'lib/images/back_enabled_hover.png',
